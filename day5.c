d2409f28414edc2855f4af0b9cd9b0c8fd383e6d
MohammedJunaid127 <mj4520040@gmail.com>
2024-11-11 15:32:41 +0530

    All prograns uploaded

---
+++ day5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int numbers[10];
+    printf("Enter 10 integers:");
+    int i = 0 ;
+    for(i=0,i<numbers;i++)
+    {
+    scanf("%d",&numbers);
+    }
+    int j = 0 ;
+    for(j=0,j<=10;j++)
+    if(i==j)
+    {
+        printf("%d",i);
+    }
+return numbers; 
+
+}
